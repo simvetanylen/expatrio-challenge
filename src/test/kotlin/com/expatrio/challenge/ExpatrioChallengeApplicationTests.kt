a2a407be92f3051956cdb6f95874685be4012574
Oreste Viron <oreste@verifeasy.com>
2021-12-14 17:35:21 +0100

    Initialize with Spring Initializr

---
+++ src/test/kotlin/com/expatrio/challenge/ExpatrioChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.expatrio.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExpatrioChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
