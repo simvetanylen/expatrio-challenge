3b4cc04a8ceb24bcd559ddef7d0733bfe03c738b
Oreste Viron <oreste@verifeasy.com>
2021-12-14 17:43:20 +0100

    Configure database

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/expatrio/challenge/App.kt
+++ src/main/kotlin/com/expatrio/challenge/App.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ExpatrioChallengeApplication
+class App
 
 fun main(args: Array<String>) {
-	runApplication<ExpatrioChallengeApplication>(*args)
+	runApplication<App>(*args)
 }
