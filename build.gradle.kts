b111fe01e41a2a4ee849dfe96662ab23cadd5460
Oreste Viron <oreste@verifeasy.com>
2021-12-14 17:37:16 +0100

    Add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
